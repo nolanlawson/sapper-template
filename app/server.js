4a29920737d3326e9198ea5e7dc4b1a8bd9f58c3
Nolan Lawson <nolan@nolanlawson.com>
2018-09-07 09:46:23 -0700

    use CSP nonce

--- app/server.js
+++ app/server.js
@@ -1,19 +1,31 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import sapper from 'sapper';
-import compression from 'compression';
-import { manifest } from './manifest/server.js';
+import sirv from 'sirv'
+import express from 'express'
+import sapper from 'sapper'
+import compression from 'compression'
+import { manifest } from './manifest/server.js'
+import helmet from 'helmet'
+import uuidv4 from 'uuid/v4'
 
-const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+const {PORT, NODE_ENV} = process.env
+const dev = NODE_ENV === 'development'
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('assets', { dev }),
-		sapper({ manifest })
-	)
-	.listen(PORT)
-	.catch(err => {
-		console.log('error', err);
-	})
+const app = express()
+
+app.use(compression({threshold: 0}))
+app.use(sirv('assets', {dev}))
+app.use(function (req, res, next) {
+	res.locals.nonce = uuidv4()
+	next()
+})
+app.use(helmet({
+	contentSecurityPolicy: {
+		directives: {
+			scriptSrc: [
+				'\'self\'',
+				(req, res) => `'nonce-${res.locals.nonce}'`
+			]
+		}
+	}
+}))
+app.use(sapper({manifest}))
+app.listen(PORT)
